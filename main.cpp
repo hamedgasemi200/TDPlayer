9f47a289b42cf1e0ce80eecc6339c1954a0e7a39
Hamed <33207312+hamedgasemi200@users.noreply.github.com>
2021-03-28 17:09:16 -0700

    Updated window title + application version

--- main.cpp
+++ main.cpp
@@ -4,6 +4,11 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    a.setApplicationVersion("1.0");
+    a.setApplicationName("Tridectet");
+    a.setApplicationDisplayName(a.applicationName() + " (" + a.applicationVersion() + ")");
+
     MainWindow w;
     w.show();
 
